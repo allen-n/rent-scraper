88863110757d7803f19ffb7f99daeb6c22b0e00c
allen-n <allennikka@gmail.com>
2021-01-30 10:36:04 -0800

    Added proxying to obfuscate IP

--- scraper.py
+++ scraper.py
@@ -8,14 +8,17 @@ from tqdm import tqdm
 import datetime
 import logging
 import statistics
+from random import randint
 
 from re import sub
 
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument("--verbose", "-v", help="True to enable verbose logging to console",
-                    type=bool, required=False, default=True)
+parser.add_argument("--verbose", "-v", help="Enable verbose logging to console", action='store_true',
+                    required=False, default=True)
+parser.add_argument("--proxy", "-p", help="Use random proxies for each scraping call. MUCH Slower but obfuscates IP Address. Also foregoes HTTPS since not all proxies support it.", action='store_true',
+                    required=False, default=False)
 parser.add_argument("--maxPages", "-m", help="Max # of pages to scrape (default 100)",
                     type=int, required=False, default=100)
 parser.add_argument("--zumperURL", "-z",
@@ -25,8 +28,8 @@ parser.add_argument("--zumperURL", "-z",
                     default="https://www.zumper.com/apartments-for-rent/san-francisco-ca/mission-dolores/2-beds?bathrooms=2&box=-122.47472544836458,37.72517967784283,-122.38697729132755,37.80021764760265")  # Bounding box is the desired area on the map
 
 args = parser.parse_args()
-
 max_pages = args.maxPages
+
 urls = ["{}&page={}"
         .format(args.zumperURL, i) for i in range(1, max_pages)]
 
@@ -35,10 +38,34 @@ def priceFromListItem(item):
     return int(sub(r"\D", "", str(item.contents)))
 
 
+def getProxyIPs():
+    r = requests.get("https://www.us-proxy.org/")
+    soup = bs(r.content, 'html.parser')
+    table = soup.find('table', attrs={"id": "proxylisttable"})
+    rows = table.findAll('tr')
+    proxies = []
+    for row in rows:
+        ip = row.findNext('td')
+        port = ip.findNext('td')
+
+        proxy = u"{}:{}".format(ip.contents[0], port.contents[0])
+        proxies.append(proxy)
+    proxies.pop()
+    return proxies
+
+
 def fetchPrices():
     all_prices = []
     for url in tqdm(urls):
-        r = requests.get(url)
+        r = None
+        if args.proxy:
+            proxies = getProxyIPs()
+            i = randint(0, len(proxies)-1)
+            r = requests.get(url, proxies={
+                "http": proxies[i]
+            })
+        else:
+            r = requests.get(url)
         if r.status_code == 404:
             print("Invalid page, ending!")
             break
@@ -53,7 +80,7 @@ def fetchPrices():
 
     # sum_price = float(sum(all_prices))
     if all_prices:
-        avg_price = statistics.mean(all_prices) # sum_price / len(all_prices)
+        avg_price = statistics.mean(all_prices)  # sum_price / len(all_prices)
         std_dev = statistics.stdev(all_prices)
         mode = None
         median = None
